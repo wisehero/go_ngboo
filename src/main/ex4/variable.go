8b2ca396ecf2c1aedf76271f36e2f1e5ac0673d0
wisehero10 <godjw96@gmail.com>
2023-01-14 02:44:45 +0900

    Go는 어떻게 변수를 선언하고 할당하는가!

---
+++ src/main/ex4/variable.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 10                    // Go 변수 선언 방식
+	var msg string = "Hello Variable" // var 변수명 타입 = 초기값
+
+	a = 20
+	msg = "Good Morning"
+	fmt.Println(msg, a)
+}
