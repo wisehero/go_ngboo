30d4e63fe1666163da8779d06aa786b86a1d865a
wisehero10 <godjw96@gmail.com>
2023-01-23 01:08:06 +0900

    큰따옴표와 백틱의 차이

--- tucker/src/main/15_string/ex1/main.go
+++ tucker/src/main/15_string/ex1/main.go
@@ -1,5 +1,11 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+	str1 := "Hello \t 'World' \n"
+	str2 := `Go is "awesome"!\nGo is simple and\t 'powerful''` // 특수문자 동작 X
+
+	fmt.Println(str1)
+	fmt.Println(str2)
 }
